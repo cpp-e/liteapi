c4aa74d3e20649eda57c31956d002da8b25352ac
Kamal Abdel Wahab <kamal.abdelwahab@gmail.com>
2022-08-19 10:04:08 +0400

    restructured and added setup ability

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='liteapi',
+    version='1.0',
+    description='Lite python API framework',
+    url='https://github.com/cpp-e/liteapi',
+    author='Kamal Abdel Wahab',
+    author_email='kamal.abdelwahab@gmail.com',
+    license='GPL-3.0',
+    packages=['liteapi']
+)
