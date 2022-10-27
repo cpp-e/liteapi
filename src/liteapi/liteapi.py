1c8d52500ef886a07456c0b86e75c894723f4884
Kamal Abdel Wahab <kamal.abdelwahab@gmail.com>
2022-10-27 21:09:09 +0400

    dist

--- src/liteapi/liteapi.py
+++ src/liteapi/liteapi.py
@@ -54,7 +54,7 @@ class liteapi:
     class __version:
         MAJOR = 0
         MINOR = 4
-        PATCH = 1
+        PATCH = 2
         
         def __str__(self):
             return str("{}.{}.{}".format(self.MAJOR, self.MINOR, self.PATCH))
